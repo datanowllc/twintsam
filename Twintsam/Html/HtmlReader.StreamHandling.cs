ff4252456a994bb5f7b266b0a908130e4a182cab
t.broyer <t.broyer@8e4777ef-e424-0410-9398-b1cd9d0e552c>
2006-12-19 13:31:26 +0000

    - Twintsam/Html/HtmlReader.StreamHandling.cs: implemented IXmlLineInfo and added stream handling methods (namely, PeekChar and EatChars; plus NextInputChar as hook for later performance improvements, as this one will be called many times)

--- Twintsam/Html/HtmlReader.StreamHandling.cs
+++ Twintsam/Html/HtmlReader.StreamHandling.cs
@@ -2,27 +2,74 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Xml;
+using System.IO;
+using System.Diagnostics;
 
 namespace Twintsam.Html
 {
     public partial class HtmlReader : IXmlLineInfo
     {
+        private const char EOF_CHAR = '\0';
+
+        private TextReader _reader;
+        private bool _readerAtEof;
+        private StringBuilder _buffer = new StringBuilder();
+
+        private int _lineNumber;
+        private int _linePosition;
+
+        private char NextInputChar { get { return PeekChar(0); } }
+
+        private char PeekChar(int offset)
+        {
+            if (_buffer.Length <= offset && !_readerAtEof) {
+                char[] chars = new char[offset - _buffer.Length + 1];
+
+                while (_buffer.Length <= offset && !_readerAtEof) {
+                    int charsToRead = Math.Min(chars.Length, offset - _buffer.Length + 1);
+
+                    int charsRead = _reader.ReadBlock(chars, 0, charsToRead);
+                    if (charsRead < charsToRead) {
+                        _readerAtEof = true;
+                    }
+
+                    _buffer.Append(chars, 0, charsRead);
+
+                    _buffer.Replace('\0', '\uFFFE');
+                    _buffer.Replace("\r\n", "\n");
+                    _buffer.Replace('\r', '\n');
+                }
+            }
+            if (_buffer.Length <= offset) {
+                return EOF_CHAR;
+            }
+            return _buffer[offset];
+        }
+
+        private void EatChars(int count)
+        {
+            char[] chars = new char[count];
+            _buffer.CopyTo(0, chars, 0, count);
+            _buffer.Remove(0, count);
+
+            Array.ForEach<char>(chars, delegate(char c)
+            {
+                if (c == '\n') {
+                    _lineNumber++;
+                    _linePosition = 0;
+                } else {
+                    _linePosition++;
+                }
+            });
+        }
+
         #region IXmlLineInfo Members
 
-        public bool HasLineInfo()
-        {
-            throw new NotImplementedException();
-        }
+        public bool HasLineInfo() { return true; }
 
-        public int LineNumber
-        {
-            get { throw new NotImplementedException(); }
-        }
+        public int LineNumber { get { return _lineNumber; } }
 
-        public int LinePosition
-        {
-            get { throw new NotImplementedException(); }
-        }
+        public int LinePosition { get { return _linePosition; } }
 
         #endregion
     }
