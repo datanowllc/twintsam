88c98cc6e8e8e4c4650ffe530cca67d51896b793
t.broyer <t.broyer@8e4777ef-e424-0410-9398-b1cd9d0e552c>
2006-12-19 17:05:31 +0000

    * Twintsam/Twintsam.csproj:  * Twintsam/Html/ContentModel.cs:  * Twintsam/Html/HtmlReader.cs:  * Twintsam/Html/HtmlReader.Tokenization.cs:  * Twintsam/Properties/Resources.*:    set up the basis of the tokenization stage

---
+++ trunk/Twintsam/Html/ContentModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Twintsam.Html
+{
+    public enum ContentModel
+    {
+        Pcdata,
+        Rcdata,
+        Cdata,
+        PlainText,
+    }
+}
