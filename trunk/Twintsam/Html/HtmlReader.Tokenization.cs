2ddf4031366ad581849cdd587c2e7615901b9019
t.broyer <t.broyer@8e4777ef-e424-0410-9398-b1cd9d0e552c>
2006-12-20 17:09:05 +0000

    * Twintsam/Html/HtmlReader.cs:  * Twintsam/Html/HtmlReader.Tokenization.cs:    - refactored end-of-stream handling    - implemented "close tag open", "bogus comment", "comment" and part of "doctype"

--- trunk/Twintsam/Html/HtmlReader.Tokenization.cs
+++ trunk/Twintsam/Html/HtmlReader.Tokenization.cs
@@ -24,17 +24,18 @@ namespace Twintsam.Html
         }
 
         /// <summary>
-        /// A tokenizing method, corresponding to a state in HTML5 tokenization stage.
+        /// A parsing function, roughly corresponding to a token to be parsed.
         /// </summary>
         /// <returns><see langword="true"/> if a token has been produced, <see langword="false"/> if no token is ready yet.</returns>
-        private delegate bool TokenizationState();
+        private delegate bool ParsingFunction();
 
-        private TokenizationState _currentTokenizationState;
+        private ParsingFunction _currentParsingFunction;
 
         private XmlNodeType _tokenType;
         private string _name;
         private string _value;
         private List<Attribute> _attributes = new List<Attribute>();
+        private bool _doctypeInError;
 
         private string _lastEmittedStartTagName;
 
@@ -59,47 +60,53 @@ namespace Twintsam.Html
 
             if (_tokenType == XmlNodeType.Element) {
                 _lastEmittedStartTagName = _name;
+            } else if (tokenType != XmlNodeType.Text) {
+                // _lastEmittedStartTagName is for CDATA and RCDATA,
+                // which can only contain text tokens, so clear it if
+                // a non-text token is produced.
+                _lastEmittedStartTagName = null;
             }
 
             _tokenType = tokenType;
             _name = null;
             _value = null;
             _attributes.Clear();
+            _doctypeInError = false;
         }
 
         /// <summary>
         /// Reads the next token from the stream.
         /// </summary>
-        /// <returns><see langword="true"/> if the next node was read successfully; <see langword="false"/> if there are no more nodes to read.</returns>
+        /// <returns><see langword="true"/> if the next token was read successfully; <see langword="false"/> if there are no more tokens to read.</returns>
         /// <remarks>
-        /// Calls the <see cref="_currentTokenisationState"/> until it returns <see langword="true"/>.
+        /// Calls the <see cref="_currentParsingFunction"/> until it returns <see langword="true"/>.
         /// </remarks>
         private bool ParseToken()
         {
-            if (_buffer.Length == 0 && _readerAtEof) {
+            if (EndOfStream) {
                 // buffer consumed and reader is at EOF => we reached EOF
                 return false;
             }
             bool newToken = false;
-            while (!newToken) {
-                newToken = _currentTokenizationState();
+            while (!newToken && !EndOfStream) {
+                newToken = _currentParsingFunction();
             }
             // Paragraph just before http://www.whatwg.org/specs/web-apps/current-work/#permitted:
             // When an end tag token is emitted with attributes, that is a parse error
-            if (_tokenType == XmlNodeType.EndElement
+            if (newToken
+                && _tokenType == XmlNodeType.EndElement
                 && _attributes != null && _attributes.Count > 0) {
                 OnParseError(Resources.Html_ParseError_EndTagWithAttributes);
             }
-            return true;
+            return newToken;
         }
 
         // http://www.whatwg.org/specs/web-apps/current-work/#data-state
         private bool ParseData()
         {
-            char c;
             StringBuilder sb = new StringBuilder();
             do {
-                c = EatNextInputChar();
+                char c = EatNextInputChar();
                 if (c == '&' && (ContentModel == ContentModel.Pcdata || ContentModel == ContentModel.Rcdata)) {
                     // http://www.whatwg.org/specs/web-apps/current-work/#entity:
                     // Entity data state is only switched to from the data state,
@@ -118,39 +125,63 @@ namespace Twintsam.Html
                     case ContentModel.Rcdata:
                     case ContentModel.Cdata:
                         if (NextInputChar == '/') {
-                            EatChars(1);
-                            // TODO: integrate "close tag open state" http://www.whatwg.org/specs/web-apps/current-work/#close1
-                            //StringBuilder tagName = new StringBuilder(_lastEmittedStartTagName.Length);
-                            //char c2 = NextInputChar;
-                            //int offset = 0;
-                            //while (offset < _lastEmittedStartTagName.Length && c2 != EOF_CHAR) {
-                            //    tagName.Append(c2);
-                            //    c2 = PeekChar(++offset);
-                            //}
-                            _currentTokenizationState = ParseEndTag;
+                            c = EatNextInputChar();
+                            // http://www.whatwg.org/specs/web-apps/current-work/#close1
+                            if (('A' <= NextInputChar && NextInputChar <= 'Z') || ('a' <= NextInputChar && NextInputChar <= 'z')) {
+                                string tagName = PeekChars(_lastEmittedStartTagName.Length);
+                                char next = PeekChar(tagName.Length);
+                                if (!String.Equals(_lastEmittedStartTagName, tagName, StringComparison.OrdinalIgnoreCase)
+                                    || (!Constants.IsSpaceCharacter(next) && next != '>' && next != '/' && next != '<' && next != EOF_CHAR)) {
+                                    OnParseError("Found end tag with different name than the previous start tag");
+                                    sb.Append("</");
+                                } else {
+                                    _currentParsingFunction = ParseEndTag;
+                                }
+                            } else if (NextInputChar == '>') {
+                                OnParseError("Empty end tag or unescaped </>");
+                            } else if (NextInputChar == EOF_CHAR) {
+                                OnParseError("Unexpected end of file");
+                                sb.Append("</");
+                                // let the next loop iteration handle EOF
+                            } else {
+                                OnParseError("Bogus comment");
+                                _currentParsingFunction = ParseBogusComment;
+                            }
                         } else {
                             sb.Append(c);
                         }
                         break;
                     case ContentModel.Pcdata:
                         if (NextInputChar == '!') {
-                            EatChars(1);
-                            _currentTokenizationState = ParseMarkupDeclaration;
+                            c = EatNextInputChar();
+                            _currentParsingFunction = ParseMarkupDeclaration;
                         } else if (NextInputChar == '/') {
-                            EatChars(1);
-                            _currentTokenizationState = ParseEndTag;
+                            c = EatNextInputChar();
+                            // http://www.whatwg.org/specs/web-apps/current-work/#close1
+                            if (('A' <= NextInputChar && NextInputChar <= 'Z') || ('a' <= NextInputChar && NextInputChar <= 'z')) {
+                                _currentParsingFunction = ParseEndTag;
+                            } else if (NextInputChar == '>') {
+                                OnParseError("Empty end tag or unescaped </>");
+                            } else if (NextInputChar == EOF_CHAR) {
+                                OnParseError("Unexpected end of file");
+                                sb.Append("</");
+                                // let the next loop iteration handle EOF
+                            } else {
+                                OnParseError("Bogus comment");
+                                _currentParsingFunction = ParseBogusComment;
+                            }
                         } else if (('A' <= NextInputChar && NextInputChar <= 'Z')
                             || ('a' <= NextInputChar && NextInputChar <= 'z')) {
                             InitToken(XmlNodeType.Element);
-                            _currentTokenizationState = ParseStartTag;
+                            _currentParsingFunction = ParseStartTag;
                         } else if (NextInputChar == '>') {
                             OnParseError("Empty tag or unescaped <>");
-                            EatChars(1);
+                            c = EatNextInputChar();
                             sb.Append("<>");
-                            _currentTokenizationState = ParseData;
+                            _currentParsingFunction = ParseData;
                         } else if (NextInputChar == '?') {
                             OnParseError("Bogus comment");
-                            _currentTokenizationState = ParseBogusComment;
+                            _currentParsingFunction = ParseBogusComment;
                         } else {
                             OnParseError("Unescaped <");
                             sb.Append(c);
@@ -162,14 +193,14 @@ namespace Twintsam.Html
                 } else {
                     sb.Append(c);
                 }
-            } while (_currentTokenizationState != ParseData);
+            } while (_currentParsingFunction == ParseData);
 
             if (sb.Length > 0) {
                 InitToken(XmlNodeType.Text);
                 _value = sb.ToString();
                 return true;
             } else {
-                return c == EOF_CHAR;
+                return false;
             }
         }
 
@@ -183,14 +214,77 @@ namespace Twintsam.Html
             throw new NotImplementedException();
         }
 
+        // http://www.whatwg.org/specs/web-apps/current-work/#bogus
         private bool ParseBogusComment()
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            for (char c = EatNextInputChar(); c != '>' && c != EOF_CHAR; c = EatNextInputChar()) {
+                sb.Append(c);
+            }
+            InitToken(XmlNodeType.Comment);
+            _value = sb.ToString();
+            return true;
         }
 
+        // http://www.whatwg.org/specs/web-apps/current-work/#markup
         private bool ParseMarkupDeclaration()
         {
-            throw new NotImplementedException();
+            if (PeekChars(2) == "--") {
+                EatChars(2);
+                // http://www.whatwg.org/specs/web-apps/current-work/#comment
+                StringBuilder sb = new StringBuilder();
+                int dashes = 0;
+                char c;
+                for (c = EatNextInputChar(); c != EOF_CHAR && (dashes < 2 || c != '>'); c = EatNextInputChar()) {
+                    if (c == '-') {
+                        dashes++;
+                    } else if (dashes >= 2 && c != '>') {
+                        // at least 2 dashes not followed by '>' => reinit dashes counter
+                        dashes = 0;
+                    }
+                    sb.Append(c);
+                }
+                InitToken(XmlNodeType.Comment);
+                _value = sb.ToString(0, sb.Length - 2);
+                if (_value.Contains("--")) {
+                    OnParseError("Comment containes double-dash");
+                } else if (_value.EndsWith("-")) {
+                    OnParseError("Comment ends on a dash: --->");
+                }
+                if (c == EOF_CHAR) {
+                    OnParseError("Unexpected end of file");
+                }
+                return true;
+            } else if (String.Equals(PeekChars(7), "DOCTYPE", StringComparison.OrdinalIgnoreCase)) {
+                EatChars(7);
+                // http://www.whatwg.org/specs/web-apps/current-work/#doctype0
+                InitToken(XmlNodeType.DocumentType);
+
+                if (!Constants.IsSpaceCharacter(NextInputChar)) {
+                    OnParseError("<!DOCTYPE should be followed by a space character, found " + NextInputChar);
+                    _doctypeInError = true;
+                    if (NextInputChar == '>' || NextInputChar == EOF_CHAR) {
+                        return true;
+                    }
+                }
+                StringBuilder sb = new StringBuilder();
+                char c = EatNextInputChar(); ;
+                while (c != EOF_CHAR && !Constants.IsSpaceCharacter(c) && c != '>') {
+                    if ('a' <= c && c <= 'z') {
+                        c = c + 0x0020;
+                    }
+                    if (c < 'A' || 'Z' < c) {
+                        _doctypeInError = true;
+                    }
+                    sb.Append(c);
+
+                    c = EatNextInputChar();
+                }
+                // TODO: after doctype name
+            } else {
+                _currentParsingFunction = ParseBogusComment;
+                return false;
+            }
         }
     }
 }
