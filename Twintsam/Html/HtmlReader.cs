1f1d33b6fb09f90c1c3b2e58ea1094d38c05832b
t.broyer <t.broyer@8e4777ef-e424-0410-9398-b1cd9d0e552c>
2006-12-21 22:33:06 +0000

    * Twintsam/Html/HtmlReader.cs: added a public constructor with a TextReader argument for testing

--- Twintsam/Html/HtmlReader.cs
+++ Twintsam/Html/HtmlReader.cs
@@ -6,6 +6,14 @@ namespace Twintsam.Html
     {
         // TODO: public constructors
 
+        public HtmlReader(TextReader reader)
+        {
+            if (reader == null) {
+                throw new ArgumentNullException("reader");
+            }
+            _reader = reader;
+        }
+
         private HtmlReader()
         {
             // http://www.whatwg.org/specs/web-apps/current-work/#tokenization:
