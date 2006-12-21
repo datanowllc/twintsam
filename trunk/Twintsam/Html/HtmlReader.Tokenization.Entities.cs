62ebb20b3e00baabbe6490561d646df76f81c976
t.broyer <t.broyer@8e4777ef-e424-0410-9398-b1cd9d0e552c>
2006-12-21 09:33:44 +0000

    * Twintsam/Html/HtmlReader.Tokenization.Entities.cs: refactored EatEntity()->string

--- trunk/Twintsam/Html/HtmlReader.Tokenization.Entities.cs
+++ trunk/Twintsam/Html/HtmlReader.Tokenization.Entities.cs
@@ -15,31 +15,7 @@ namespace Twintsam.Html
         private const int MAX_UNICODE_CODEPOINT_HEXDIGITS = 6;
         private const int MAX_UNICODE_CODEPOINT_DIGITS = 9;
 
-        private static readonly Dictionary<string, string> EntityNamePrefixes = new Dictionary<string, string>(HtmlEntities.NumberOfEntities);
-
-        [SuppressMessage("Microsoft.Performance", "CA1810:InitializeReferenceTypeStaticFieldsInline")]
-        static HtmlReader()
-        {
-            // FIXME: that's really not optimized
-            foreach (string entityName in HtmlEntities.EntityNames) {
-                // Start with 'length-2' as we only want prefixes
-                for (int length = entityName.Length - 2; length > 0; length--) {
-                    string prefix = entityName.Substring(0, length);
-                    EntityNamePrefixes[prefix] = prefix;
-                }
-            }
-        }
-
-        private static bool IsEntityNamePrefix(string s)
-        {
-            Debug.Assert(!String.IsNullOrEmpty(s));
-            Debug.Assert(s.Length < HtmlEntities.LonguestEntityNameLength);
-
-            return EntityNamePrefixes.ContainsKey(s);
-        }
-
         // http://www.whatwg.org/specs/web-apps/current-work/#consume
-        [SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         private string EatEntity()
         {
             // NOTE: the ampersand has already been eaten
@@ -54,44 +30,41 @@ namespace Twintsam.Html
                     next = PeekChar(3);
                 }
                 if (next < '0' || '9' < next) {
-                    OnParseError("not a numeric entity");
+                    OnParseError("Unescaped &#" + (isHex ? "x" : ""));
                     return null;
                 }
                 // From this point, we know we at least have a "well-formed" numeric entity, so we can safely consume '&#x' characters
                 EatChars(isHex ? 3 : 2);
 
-                int offset = 0;
-                // Skip leading zeros
-                do {
-                    next = PeekChar(++offset);
-                } while (next == '0');
-                // collect digits
-                StringBuilder sb = new StringBuilder(MAX_UNICODE_CODEPOINT_DIGITS); // use max-digits, as base-10 numeric strings are always longer than base-16 ones
-                while (('0' <= next && next <= '9') || (isHex && ('A' <= next && next <= 'F') || ('a' <= next && next <= 'f'))) {
-                    sb.Append((char)next);
+                SkipChars(delegate(char c) { return c == '0'; });
 
-                    next = PeekChar(++offset);
-                }
-                if (next != ';') {
-                    OnParseError("the entity does not end with a semi-colon");
+                string digits = PeekChars(delegate(char c) {
+                    return ('0' <= next && next <= '9') || (isHex && ('A' <= next && next <= 'F') || ('a' <= next && next <= 'f'));
+                }, MAX_UNICODE_CODEPOINT_DIGITS); // base-10 numeric strings are always longer than base-16 ones for the same value
+
+                int length = digits.Length;
+                if (PeekChar(length) == ';') {
+                    length++;
                 } else {
-                    offset++;
+                    OnParseError("Entity does not end with a semi-colon");
                 }
-                EatChars(offset);
+                EatChars(length);
+
                 int codepoint;
-                if (sb.Length == 0) {
+                if (digits.Length == 0) {
                     // given that we skipped leading zeros, this means there were only zeros
                     codepoint = REPLACEMENT_CHAR;
-                } else if (sb.Length > MAX_UNICODE_CODEPOINT_HEXDIGITS) {
-                    OnParseError("too many hex-digits, cannot be a valid Unicode character");
+                } else if (digits.Length > MAX_UNICODE_CODEPOINT_HEXDIGITS) {
+                    OnParseError("Too many hex-digits, it cannot be a valid Unicode character");
                     codepoint = REPLACEMENT_CHAR;
                 } else {
-                    codepoint = Int32.Parse(sb.ToString(), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+                    codepoint = Int32.Parse(digits, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
                     if (codepoint <= 0 || codepoint > MAX_UNICODE_CODEPOINT) {
-                        OnParseError("Invalid Unicode character");
+                        OnParseError("Invalid Unicode character &#" + (isHex ? "x" : "") + digits + ";");
                         codepoint = REPLACEMENT_CHAR;
                     }
                 }
+
                 if (128 <= codepoint && codepoint <= 159) {
                     return Windows1252Encoding.GetString(new byte[] { (byte)codepoint });
                 }
@@ -101,34 +74,41 @@ namespace Twintsam.Html
                     return null;
                 }
             } else {
-                int offset = 0;
-                StringBuilder sb = new StringBuilder(HtmlEntities.LonguestEntityNameLength);
-                int foundChar = -1;
-                int foundEntityNameLength = -1;
-                do {
-                    int next = PeekChar(++offset);
-                    if (next < 0 || next == ';') {
-                        break;
+                string entityName = PeekChars(HtmlEntities.LonguestEntityNameLength);
+                entityName = entityName.Substring(0, entityName.IndexOf(';'));
+
+                if (entityName.Length == 0) {
+                    if (NextInputChar == EOF_CHAR) {
+                        OnParseError("Unexpected end of file in character entity");
+                    } else {
+                        OnParseError("Empty entity name &;");
                     }
-                    sb.Append(Char.ConvertFromUtf32(next));
-                    if (sb.Length >= HtmlEntities.ShortestEntityNameLength) {
-                        int c;
-                        if (HtmlEntities.TryGetChar(sb.ToString(), out c)) {
-                            foundChar = c;
-                            foundEntityNameLength = offset;
-                        }
-                    }
-                } while (sb.Length < HtmlEntities.LonguestEntityNameLength && IsEntityNamePrefix(sb.ToString()));
-                if (foundChar < 0) {
-                    OnParseError("we didn't find any matching entity");
                     return null;
                 }
-                if (PeekChar(foundEntityNameLength) != ';') {
-                    OnParseError("the entity does not end with a semi-colon");
-                } else {
-                    offset++;
+
+                int foundChar = -1;
+                while (foundChar < 0 && entityName.Length >= HtmlEntities.ShortestEntityNameLength) {
+                    int c;
+                    if (HtmlEntities.TryGetChar(entityName, out c)) {
+                        foundChar = c;
+                    } else {
+                        entityName = entityName.Substring(0, entityName.Length - 1);
+                    }
                 }
-                EatChars(offset);
+
+                if (foundChar < 0) {
+                    OnParseError("Named entity not found: " + entityName);
+                    return null;
+                }
+
+                int length = entityName.Length;
+                if (PeekChar(length) == ';') {
+                    length++;
+                } else {
+                    OnParseError("Entity does not end with a semi-colon");
+                }
+                EatChars(length);
+
                 return Char.ConvertFromUtf32(foundChar);
             }
         }
