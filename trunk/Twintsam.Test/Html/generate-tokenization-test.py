8402b223e81c7569b07ef1d691a3e0603bd5658f
t.broyer <t.broyer@8e4777ef-e424-0410-9398-b1cd9d0e552c>
2006-12-22 11:05:05 +0000

    * Twintsam.Test: added svn:externals to automatically retrieve test cases from the python html5lib project SVN repository
     * Twintsam.Test/Twintsam.Test.csproj:
     * Twintsam.Test/Html/generate-tokenization-test.py:
     * Twintsam.Test/Html/generate-tree-construction-test.py:
       customized build process to re-generate unit-test C# code from html5lib's test cases
    
     * Twintsam.Test/Html: added svn:ignore to ignore auto-generated unit-test C# code files

---
+++ trunk/Twintsam.Test/Html/generate-tokenization-test.py
@@ -0,0 +1,80 @@
+import sys
+import codecs
+import simplejson
+
+tests = simplejson.load(file(sys.argv[1]))
+
+output = codecs.open(sys.argv[2], 'w', 'utf-8')
+
+output.write("""
+#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#else
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
+using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+#endif
+
+using System;
+using System.Collections.Generic;
+
+namespace Twintsam.Html
+{
+    public partial class HtmlReaderTest
+    {
+""")
+
+i = 0
+for test in tests['tests']:
+	description = test['description'].replace('"', '\"')
+	output.write("""
+#if !NUNIT
+	[TestMethod]
+	[Description("%s")]
+#else
+	[TestMethod(Description="%s")]
+#endif
+	public void Test%d()
+	{
+		DoTest("%s", new object[] {
+	""" % (description, description, i, test['input']))
+	
+	for token in test['output']:
+		if token in ('ParseError', 'AtheistParseError'):
+			output.write('"%s", ' % token)
+		elif token[0] == 'DOCTYPE':
+			output.write('new object[] { "DOCTYPE", "%s", %s }, ' % (token[1], token[2] and 'true' or 'false'))
+		elif token[0] == 'StartTag':
+			output.write('new object[] { "StartTag", "%s", ' % token[1])
+			if token[2]:
+				output.write('new KeyValuePair<string,string>[] { ')
+				for key, value in token[2].iteritems():
+					output.write('new KeyValuePair<string,string>("%s", "%s"), ' % (key, value))
+				output.write(' } ')
+			else:
+				output.write('null')
+			output.write(' }, ')
+		elif token[0] == 'EndTag':
+			output.write('new string[] { "EndTag", "%s" }, ' % token[1])
+		elif token[0] == 'Comment':
+			output.write('new string[] { "Comment", "%s" }, ' % token[1])
+		elif token[0] == 'Character':
+			output.write('new string[] { "Character", "%s" }, ' % token[1])
+	
+	output.write("""
+		});
+	}
+	""")
+	i += 1
+
+output.write("""
+	}
+}
+""")
+
+output.flush()
+output.close()
