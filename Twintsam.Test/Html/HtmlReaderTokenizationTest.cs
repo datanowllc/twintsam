de401f2d87b434db2c73ab2a72aa0c73bc87dcc4
t.broyer <t.broyer@8e4777ef-e424-0410-9398-b1cd9d0e552c>
2006-12-27 10:55:38 +0000

    * Twintsam.Test/Html/HtmlReaderTokenizationTest.cs: finished DoTest: lacked comparison of 'expectedOutput' and 'actualOutput' !!! Also changed the ParseError event handler to "merge" consecutive parse errors.

--- Twintsam.Test/Html/HtmlReaderTokenizationTest.cs
+++ Twintsam.Test/Html/HtmlReaderTokenizationTest.cs
@@ -15,6 +15,8 @@ using System.Reflection;
 using System.Collections;
 using System.IO;
 using System.Xml;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Twintsam.Html
 {
@@ -45,10 +47,12 @@ namespace Twintsam.Html
 
         private void reader_ParseError(object source, ParseErrorEventArgs args)
         {
-            actualOutput.Add("ParseError");
+            if (actualOutput.Count == 0 || actualOutput[actualOutput.Count - 1] as string != "ParseError") {
+                actualOutput.Add("ParseError");
+            }
         }
 
-        private void DoTest(string input, IEnumerable expectedOutput)
+        private void DoTest(string input, IList expectedOutput)
         {
             HtmlReader reader = new HtmlReader(new StringReader(input));
             reader.ParseError += new EventHandler<ParseErrorEventArgs>(reader_ParseError);
@@ -65,28 +69,92 @@ namespace Twintsam.Html
                     actualOutput.Add(new object[] { "DOCTYPE", name, doctypeInError });
                     break;
                 case XmlNodeType.Element:
-                    Hashtable attrs = new Hashtable();
+                    Dictionary<string,string> attrs = new Dictionary<string,string>(StringComparer.InvariantCulture);
                     foreach (object attr in attributes) {
                         attrs.Add(
-                            HtmlReader_Attribute_name.GetValue(attr),
-                            HtmlReader_Attribute_value.GetValue(attr));
+                            (string)HtmlReader_Attribute_name.GetValue(attr),
+                            (string)HtmlReader_Attribute_value.GetValue(attr));
                     }
                     actualOutput.Add(new object[] { "StartTag", name, attrs });
                     break;
                 case XmlNodeType.EndElement:
-                    actualOutput.Add(new object[] { "EndTag", name });
+                    actualOutput.Add(new string[] { "EndTag", name });
                     break;
                 case XmlNodeType.Comment:
-                    actualOutput.Add(new object[] { "Comment", value });
+                    actualOutput.Add(new string[] { "Comment", value });
                     break;
                 case XmlNodeType.Text:
-                    actualOutput.Add(new object[] { "Character", value });
+                    actualOutput.Add(new string[] { "Character", value });
                     break;
                 default:
                     Assert.Fail("Unexpected token type: {0}", nodeType);
                     break;
                 }
             }
+
+            Trace.Write("Expected output: ");
+            TraceOutput(expectedOutput);
+            Trace.WriteLine("");
+            Trace.Write("Actual output: ");
+            TraceOutput(actualOutput);
+            Trace.WriteLine("");
+
+            Assert.AreEqual(expectedOutput.Count, actualOutput.Count, "Not the same number of tokens");
+
+            for (int i = 0; i < expectedOutput.Count; i++) {
+                object expected = expectedOutput[i];
+                object actual = actualOutput[i];
+
+                Assert.AreEqual(expected.GetType(), actual.GetType());
+
+                if (expected.GetType() != typeof(string)) {
+                    object[] expectedToken = (object[])expected;
+                    object[] actualToken = (object[])actual;
+
+                    Assert.AreEqual(expectedToken.Length, actualToken.Length);
+
+                    for (int j = 0; j < expectedToken.Length; j++) {
+                        if (expectedToken[j] is ICollection<KeyValuePair<string, string>>) {
+                            Assert.IsInstanceOfType(typeof(IDictionary<string, string>), actualToken[j]);
+
+                            ICollection<KeyValuePair<string, string>> expectedDict = (ICollection<KeyValuePair<string, string>>)expectedToken[j];
+                            IDictionary<string, string> actualDict = (IDictionary<string, string>)actualToken[j];
+
+                            Assert.AreEqual(expectedDict.Count, actualDict.Count);
+
+                            foreach (KeyValuePair<string, string> attr in expectedDict) {
+                                Assert.AreEqual(attr.Value, actualDict[attr.Key]);
+                            }
+                        } else {
+                            Assert.AreEqual(expectedToken[j], actualToken[j]);
+                        }
+                    }
+                }
+            }
+        }
+
+        [Conditional("TRACE")]
+        private void TraceOutput(ICollection collection)
+        {
+            Trace.Write("{ ");
+            foreach (object obj in collection) {
+                if (obj is ICollection) {
+                    TraceOutput((ICollection)obj);
+                } else if (obj is KeyValuePair<string,string>) {
+                    KeyValuePair<string, string> pair = (KeyValuePair<string, string>)obj;
+                    Trace.Write('"');
+                    Trace.Write(pair.Key);
+                    Trace.Write("\"=\"");
+                    Trace.Write(pair.Value);
+                    Trace.Write('"');
+                } else {
+                    Trace.Write('"');
+                    Trace.Write(obj);
+                    Trace.Write('"');
+                }
+                Trace.Write(", ");
+            }
+            Trace.Write("}");
         }
 
         [TestInitialize]
