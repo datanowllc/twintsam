5caa7b1322a7819307cf9c835c9d1476cc761aa1
t.broyer <t.broyer@8e4777ef-e424-0410-9398-b1cd9d0e552c>
2006-12-29 08:22:20 +0000

    * Twintsam.Test/Html/HtmlReaderTest.cs:  * Twintsam.Test/Twintsam.Test.csproj:  * Twintsam.Test/Twintsam.Test_VSTS.csproj:    added HtmlReaderTest.cs for tests other than tokenization and tree-construction (e.g. arguments preconditions, InvalidOperationException depending on ReadState, etc.)

---
+++ Twintsam.Test/Html/HtmlReaderTest.cs
@@ -0,0 +1,29 @@
+#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#else
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
+using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+#endif
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Twintsam.Html
+{
+    [TestClass]
+    public class HtmlReaderTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorThrowsExceptionOnNullArgument()
+        {
+            new HtmlReader(null);
+        }
+    }
+}
