6d290b7c37acb40d4c94a8af6b913e5967799e0e
t.broyer <t.broyer@8e4777ef-e424-0410-9398-b1cd9d0e552c>
2007-07-27 21:20:45 +0000

    Added PreambleDetector (for BOM sniffing)

---
+++ Twintsam/IO/PreambleDetector.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Twintsam.IO
+{
+    public static class PreambleDetector
+    {
+        private const int DEFAULT_BUFFER_SIZE = 3;
+
+        public static readonly Encoding[] DetectedEncodings = {
+            Encoding.BigEndianUnicode,
+            Encoding.Unicode,
+            Encoding.UTF8,
+        };
+
+        public static Encoding Detect(Stream stream)
+        {
+            return Detect(stream, DetectedEncodings, DEFAULT_BUFFER_SIZE);
+        }
+
+        public static Encoding Detect(Stream stream, IEnumerable<Encoding> encodings, int bufferSize)
+        {
+            if (stream == null) {
+                throw new ArgumentNullException("stream");
+            }
+            if (encodings == null) {
+                throw new ArgumentNullException("encodings");
+            }
+            if (!stream.CanSeek && !stream.CanRead) {
+                throw new ArgumentException();
+            }
+
+            foreach (Encoding encoding in encodings) {
+                stream.Seek(0, SeekOrigin.Begin);
+                bool found = true;
+                foreach (byte b in encoding.GetPreamble()) {
+                    if (b != stream.ReadByte()) {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found) {
+                    return encoding;
+                }
+            }
+            stream.Seek(0, SeekOrigin.Begin);
+            return null;
+        }
+    }
+}
