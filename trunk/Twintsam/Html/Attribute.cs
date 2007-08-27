ee664262fc3f9ef4d0817ade4ac08b9587222f8b
t.broyer <t.broyer@8e4777ef-e424-0410-9398-b1cd9d0e552c>
2007-08-27 18:07:33 +0000

    Oops, forgot to "svn add" some files...

---
+++ trunk/Twintsam/Html/Attribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+
+namespace Twintsam.Html
+{
+    internal class Attribute : IXmlLineInfo
+    {
+        public string name;
+        public string value = "";
+        public char quoteChar = ' ';
+
+        private bool _hasLineInfo;
+        private int _lineNumber;
+        private int _linePosition;
+
+        public Attribute(string name, IXmlLineInfo lineInfo)
+        {
+            this.name = name;
+
+            _hasLineInfo = lineInfo.HasLineInfo();
+            _lineNumber = lineInfo.LineNumber;
+            _linePosition = lineInfo.LinePosition;
+        }
+
+        #region IXmlLineInfo Members
+        public bool HasLineInfo() { return _hasLineInfo; }
+        public int LineNumber { get { return _lineNumber; } }
+        public int LinePosition { get { return _linePosition; } }
+        #endregion
+    }
+}
